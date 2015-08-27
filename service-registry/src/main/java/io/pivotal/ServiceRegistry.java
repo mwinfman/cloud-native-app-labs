0cc806d73e38fc6b44d98cdbac0564abb0f570fe
Dave Roberts <droberts@gopivotal.com>
2015-08-26 21:47:36 -0500

    renamed

--- service-registry/src/main/java/io/pivotal/ServiceRegistry.java
+++ service-registry/src/main/java/io/pivotal/ServiceRegistry.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class ServiceRegistryServer {
+public class ServiceRegistry {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceRegistryServer.class, args);
+        SpringApplication.run(ServiceRegistry.class, args);
     }
 }
