ddfa9fee4374494aa2a56a493ddeb64644c2c609
Dave Roberts <droberts@gopivotal.com>
2015-08-30 13:10:45 -0500

    adding config-client project

---
+++ greeting-config/src/test/java/io/pivotal/ConfigClientExampleApplicationTests.java
@@ -0,0 +1,20 @@
+package io.pivotal;
+
+import io.pivotal.ConfigClient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ConfigClient.class)
+@WebAppConfiguration
+public class ConfigClientExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
