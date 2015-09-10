417f71dee3abf3c424a49637fc26614a92df8f02
Dave Roberts <droberts@gopivotal.com>
2015-09-10 16:40:53 -0500

    refactoring to use ribbon

--- greeting-hystrix/src/main/java/io/pivotal/fortune/FortuneService.java
+++ greeting-hystrix/src/main/java/io/pivotal/fortune/FortuneService.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.discovery.DiscoveryClient;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @Service
@@ -17,13 +15,11 @@ public class FortuneService {
 			.getLogger(FortuneService.class);
 	
 	@Autowired
-	private DiscoveryClient discoveryClient;
-	
-	
+	private RestTemplate restTemplate;
+		
 	@HystrixCommand(fallbackMethod = "defaultFortune")
 	public String getFortune() {
-		RestTemplate restTemplate = new RestTemplate();
-        String fortune = restTemplate.getForObject(fetchFortuneServiceUrl(), String.class);
+        String fortune = restTemplate.getForObject("http://fortune-service", String.class);
 		return fortune;
 	}
 	
@@ -32,14 +28,6 @@ public class FortuneService {
 		return "This fortune is no good. Try another.";
 	}
 	
-	private String fetchFortuneServiceUrl() {
-	    InstanceInfo instance = discoveryClient.getNextServerFromEureka("FORTUNE-SERVICE", false);
-	    logger.debug("instanceID: {}", instance.getId());
-
-	    String fortuneServiceUrl = instance.getHomePageUrl();
-		logger.debug("fortune service url: {}", fortuneServiceUrl);
-
-	    return fortuneServiceUrl;
-	}		
+	
 	
 }
